1f9d89510e8e0f3a8f53745cec09fd1706663569
Vadim Platonov <vadim.platonov@tuloscapital.com>
2015-03-04 07:47:49 +0100

    Version 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure.joda-time "0.4.0-SNAPSHOT"
+(defproject clojure.joda-time "0.4.0"
   :description "Idiomatic Clojure wrapper for Joda-Time"
   :url "http://github.com/dm3/clojure.joda-time"
   :license {:name "MIT License"
