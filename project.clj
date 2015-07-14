06d9f6b748536d18419a4f75d4de7dbab9197eb4
Vadim Platonov <vadim.platonov@tuloscapital.com>
2015-07-14 12:10:17 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -9,15 +9,15 @@
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
                                   [org.clojure/test.check "0.5.8"]
                                   [criterium "0.4.2"]]
-                   :plugins [[codox "0.8.9"]]
-                   :codox {:include [joda-time joda-time.purgatory]}
+                   :plugins [[codox "0.8.13"]]
+                   :codox {:include [joda-time joda-time.purgatory joda-time.accessors]}
                    :source-paths ["dev"]
                    :global-vars {*warn-on-reflection* true}}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :aliases {"test-all" ["with-profile" "dev,default:dev,1.4,default:dev,1.5,default:dev,1.7,default" "test"]}
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :sign-releases false}]])
