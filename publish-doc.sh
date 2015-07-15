5622fabe987a96eed8d6d13d6e155d7dffb205fb
Vadim Platonov <vadim.platonov@tuloscapital.com>
2015-07-15 07:46:29 +0200

    update doc publish script

--- publish-doc.sh
+++ publish-doc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-lein doc && cd doc \
-    && git checkout gh-pages && \
-    git add . && git commit -am "Documentation update" \
+lein doc \
+    && git checkout gh-pages && rm -rf ./css ./js *.html && mv ./doc/* .\
+    && git add . && git commit -am "Documentation update" \
     && git push -u origin gh-pages && \
     cd ..
